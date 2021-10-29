59bf3e1047fa57eeb749402f66db2dfb13e145dc
Florencekyarikunda <florencekyarikund@gmail.com>
2021-10-29 08:20:13 +0300

    addedhost

--- news/settings.py
+++ news/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'news.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
